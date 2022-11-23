641165e3a056a7481c6d2660db7b3fe05c4def42
AmandinhaMatta <mattavieiraamanda@gmail.com>
2022-11-23 00:40:52 -0300

    REV.5

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,27 @@
-$('section.awSlider .carousel').carousel({
-    pause: "hover",
-    interval: 2000
+const slider = document.querySelector('.gallery');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', e => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
 });
-
-var startImage = $('section.awSlider .item.active > img').attr('src');
-$('section.awSlider').append('<img src="' + startImage + '">');
-
-$('section.awSlider .carousel').on('slid.bs.carousel', function () {
-    var bscn = $(this).find('.item.active > img').attr('src');
-    $('section.awSlider > img').attr('src', bscn);
+slider.addEventListener('mouseleave', _ => {
+  isDown = false;
+  slider.classList.remove('active');
 });
-
-
-/* 
-Philips ambilight tv
-Üzerine gleince duruyor slide
-*/
+slider.addEventListener('mouseup', _ => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', e => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  slider.scrollLeft = scrollLeft - walk;
+});
